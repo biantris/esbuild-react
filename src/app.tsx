0917a0b03764f5df1ff371405883d819ebbc65d4
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2021-11-13 22:20:06 -0300

    feat(init):  feat(init): ༼ つ ◕_◕ ༽つ esbuild-react

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import React, { useCallback, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props: {message: string}) => {
+  const [count, setCount] = useState(0);
+
+  const increment = useCallback(() => setCount(count + 1), [count]);
+  const decrement = useCallback(() => setCount(count - 1), [count]);
+
+  return (
+    <>
+    <h1>Hello Word Esbuild + React + TypeScript </h1>
+      <p>{count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
+  );
+};
+
+ReactDOM.render(
+<App message="Hello, world!" />, 
+document.getElementById('root')
+);
